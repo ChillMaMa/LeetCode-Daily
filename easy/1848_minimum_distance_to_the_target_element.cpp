f7be6ab85c9c884d1cbe9f9693befadcd5efd3a2
ChillMa <e1981042925@gmail.com>
2026-04-19 17:18:17 +0800

    feat: solve #1848. Minimum Distance to the Target Element

---
+++ easy/1848_minimum_distance_to_the_target_element.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=1848 lang=cpp
+ *
+ * [1848] Minimum Distance to the Target Element
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int offset = 0;
+        bool leftBound = false;
+        bool rightBound = false;
+        
+        while(true) {
+            if(!leftBound && nums[start - offset] == target) {
+                return offset;
+            }
+            else if((start - offset) == 0) {
+                leftBound = true;
+            }
+
+            if(!rightBound && nums[start + offset] == target) {
+                return offset;
+            }
+            else if((start + offset) == nums.size() - 1) {
+                rightBound = true;
+            }
+
+            offset++;
+        }
+    }
+};
+// @lc code=end
+
