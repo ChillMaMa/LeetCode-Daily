39953ca48c649c9517ec9473173d9949e6821513
ChillMa <e1981042925@gmail.com>
2026-04-19 17:41:32 +0800

    refactor: optimize #1848. Minimum Distance to the Target Element

--- easy/1848_minimum_distance_to_the_target_element.cpp
+++ easy/1848_minimum_distance_to_the_target_element.cpp
@@ -8,28 +8,14 @@
 class Solution {
 public:
     int getMinDistance(vector<int>& nums, int target, int start) {
-        int offset = 0;
-        bool leftBound = false;
-        bool rightBound = false;
-        
-        while(true) {
-            if(!leftBound && nums[start - offset] == target) {
-                return offset;
+        int index = -1;
+        int minDist = INT_MAX;
+        while(++index < nums.size()) {
+            if(nums[index] == target) {
+                minDist = min(minDist, abs(index - start));
             }
-            else if((start - offset) == 0) {
-                leftBound = true;
-            }
-
-            if(!rightBound && nums[start + offset] == target) {
-                return offset;
-            }
-            else if((start + offset) == nums.size() - 1) {
-                rightBound = true;
-            }
-
-            offset++;
         }
+        return minDist;
     }
 };
 // @lc code=end
-
