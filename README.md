1c98995cc71cef91c996d5cb8b755fe52f4c1882
ChillMa <e1981042925@gmail.com>
2026-04-19 16:56:31 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# LeetCode Daily" 
+
+feat: solve #100 Same Tree
+docs: update README with progress (修改筆記)
+refactor: optimize #1 Two Sum to O(n) (優化舊代碼)
---
+++ inc/leetcode.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
